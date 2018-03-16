9864722465b7594b3885a7ff19684b5b907fa996
mzeng7 <mzeng7@berkeley.edu>
2018-03-16 00:01:33 -0700

    implement board, move, coordinate

--- player.py
+++ player.py
@@ -8,18 +8,21 @@ class Color(Enum):
 class Move:
     def __init__(self, move):
         # move is a string of the format rowcol-rowcol[q]
-        self.start
-        self.end
-        self._quantum
+        self.move = move
+        self.start = Coordinate(move[0], move[1])
+        self.end = Coordinate(move[3], move[4])
+        self._quantum = len(move) > 5
 
     def __str__(self):
         #TODO: algebraic notation
-        return
+        return self.move
 
 class Player:
-    def __init__(self, color=Color.WHITE):
+    def __init__(self, name, color=Color.WHITE):
         # add whatever characteristics you want
         # e.g. name, rating, etc.
+        assert isinstance(name, str)
+        self.name = name
         self._side = color
 
     def get_move(self, board):
